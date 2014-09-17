d97a990e2dfff266141d1a0fafb9d062f0a0a2aa
Nilhcem <nilhcem@gmail.com>
2014-09-17 17:13:57 +0200

    Add Twitter Share Intent

--- app/src/main/java/com/nilhcem/selfid/core/selfie/PhotoSaver.java
+++ app/src/main/java/com/nilhcem/selfid/core/selfie/PhotoSaver.java
@@ -1,7 +1,10 @@
 package com.nilhcem.selfid.core.selfie;
 
+import android.content.ActivityNotFoundException;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.hardware.Camera;
@@ -13,13 +16,14 @@ import com.nilhcem.selfid.core.utils.BitmapUtils;
 import com.nilhcem.selfid.core.utils.FsUtils;
 
 import java.io.File;
+import java.util.List;
 
 public class PhotoSaver implements Camera.PictureCallback {
 
     private final Context mContext;
 
     public PhotoSaver(Context context) {
-        mContext = context.getApplicationContext();
+        mContext = context;
     }
 
     @Override
@@ -42,5 +46,31 @@ public class PhotoSaver implements Camera.PictureCallback {
 
         // Force the MediaScanner to add the file (so it is visible on the gallery)
         mContext.sendBroadcast(new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE, Uri.fromFile(output)));
+
+        // Twitter share intent
+        startTwitterShareIntent(output);
+    }
+
+    private void startTwitterShareIntent(File file) {
+        try {
+            Intent intent = new Intent(Intent.ACTION_SEND);
+            intent.putExtra(Intent.EXTRA_TEXT, mContext.getString(R.string.twitter_share_msg));
+            intent.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(file));
+            intent.setType("image/jpeg");
+
+            PackageManager pm = mContext.getPackageManager();
+            List<ResolveInfo> infos = pm.queryIntentActivities(intent, PackageManager.MATCH_DEFAULT_ONLY);
+            boolean resolved = false;
+            for (ResolveInfo ri : infos) {
+                if (ri.activityInfo.name.contains("twitter")) {
+                    intent.setClassName(ri.activityInfo.packageName, ri.activityInfo.name);
+                    resolved = true;
+                    break;
+                }
+            }
+            mContext.startActivity(resolved ? intent : Intent.createChooser(intent, mContext.getString(R.string.twitter_share_intent_chooser_title)));
+        } catch (final ActivityNotFoundException e) {
+            Toast.makeText(mContext, R.string.twitter_not_found, Toast.LENGTH_SHORT).show();
+        }
     }
 }
