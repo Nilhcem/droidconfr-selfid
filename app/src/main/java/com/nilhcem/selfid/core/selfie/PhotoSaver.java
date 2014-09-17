507ecbc649ab6b41e6a4cece239215219fcc1262
Nilhcem <nilhcem@gmail.com>
2014-09-17 16:32:43 +0200

    Force the MediaScanner to scan the generated file

--- app/src/main/java/com/nilhcem/selfid/core/selfie/PhotoSaver.java
+++ app/src/main/java/com/nilhcem/selfid/core/selfie/PhotoSaver.java
@@ -1,9 +1,11 @@
 package com.nilhcem.selfid.core.selfie;
 
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.hardware.Camera;
+import android.net.Uri;
 import android.widget.Toast;
 
 import com.nilhcem.selfid.R;
@@ -32,10 +34,13 @@ public class PhotoSaver implements Camera.PictureCallback {
         byte[] result = BitmapUtils.toByteArray(BitmapUtils.overlay(bitmap, layer));
 
         // Save everything
-        File output = FsUtils.generateFileName("orig");
+        File output = FsUtils.generateFileName("generated");
         if (output == null || !FsUtils.saveDataToFile(result, output)) {
             Toast.makeText(mContext, R.string.selfie_error_save, Toast.LENGTH_LONG).show();
             return;
         }
+
+        // Force the MediaScanner to add the file (so it is visible on the gallery)
+        mContext.sendBroadcast(new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE, Uri.fromFile(output)));
     }
 }
