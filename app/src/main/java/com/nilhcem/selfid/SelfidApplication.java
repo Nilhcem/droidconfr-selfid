4d9f40f1d9f5f1d9f9cd1a999845ade2f65d124a
Gautier Nilhcem <nilhcem@gmail.com>
2015-03-12 16:23:01 +0100

    Update dependencies

--- app/src/main/java/com/nilhcem/selfid/SelfidApplication.java
+++ app/src/main/java/com/nilhcem/selfid/SelfidApplication.java
@@ -2,8 +2,6 @@ package com.nilhcem.selfid;
 
 import android.app.Application;
 
-import com.nilhcem.selfid.core.log.ReleaseLogTree;
-
 import timber.log.Timber;
 
 public class SelfidApplication extends Application {
@@ -11,14 +9,9 @@ public class SelfidApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        initLogger();
-    }
 
-    private void initLogger() {
         if (BuildConfig.DEBUG) {
             Timber.plant(new Timber.DebugTree());
-        } else {
-            Timber.plant(new ReleaseLogTree());
         }
     }
 }
